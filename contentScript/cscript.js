2bf16d736f388edc30a52bc206844a4b72c43650
J3000 <j3000@bk.ru>
2023-09-29 15:48:34 +0300

    feat: setup.

---
+++ contentScript/cscript.js
@@ -0,0 +1,85 @@
+﻿let videoData;
+
+main();
+
+function main() {
+    const title = /https:\/\/(?<title>.*?)\/.*?/.exec(window.location).groups["title"];
+    console.log(`INFO: Content script injected to <${title}>.`);
+
+    switch (title) {
+        case "amedia.online":
+            amediaExec();
+            break;
+        case "mangavost.org":
+            mangavostExec();
+            break;
+    }
+}
+
+function amediaExec() {
+    const firstEl = document.querySelector(".playlist-episodes > a:nth-of-type(1)");
+    const secondEl = document.querySelector(".playlist-episodes > a:nth-of-type(2)");
+
+    if (secondEl) {
+        if (getTitle(secondEl) === "Следующая") {
+            setPrevNextLinks(firstEl.href, secondEl.href);
+        } else {
+            if (getTitle(firstEl) === "Предыдущая") {
+                setPrevNextLinks(firstEl.href, null);
+            } else {
+                setPrevNextLinks(null, firstEl.href);
+            }
+        }
+    } else {
+        setPrevNextLinks(null, null);
+    }
+
+}
+
+function getTitle(element) {
+    return /^(?<title>[А-Яа-я]+)\s\d+\s[А-Яа-я]+/.exec(element.textContent)?.groups.title;
+}
+
+function setPrevNextLinks(prev, next) {
+    window.addEventListener("message", (msg) => {
+        if (msg.origin.startsWith("https://mangavost.org") && msg.data.request === "get_video_data") {
+            console.log(`INFO: Message recieved. Request <${msg.data.request}>.`);
+            const name = document.querySelector(".titlesp + a").text;
+            msg.source.postMessage({
+                request: "post_video_data",
+                name: name,
+                previousEpisode: prev,
+                nextEpisode: next
+            },
+                msg.origin);
+
+            console.log(`INFO: Message sent to <${msg.origin}>. Data <${JSON.stringify(msg.data)}>.`);
+        }
+    });
+
+    console.log(`INFO: Listener set.`);
+}
+
+function mangavostExec() {
+    window.addEventListener("message", (msg) => {
+        if (msg.origin.startsWith("https://amedia.online") && msg.data.request === "post_video_data") {
+            console.log(`INFO: Message recieved. Request <${msg.data.request}>.`);
+            videoData = msg.data;
+            console.log(`INFO: Data saved. Data <${JSON.stringify(videoData)}>.`);
+        }
+    });
+
+    console.log(`INFO: Listener set.`);
+
+    window.parent.postMessage({ request: "get_video_data" }, "https://amedia.online/*");
+
+    console.log(`INFO: Message sent. Request <get_video_data>.`);
+
+    setupVideoControls();
+}
+
+function setupVideoControls() {
+
+
+
+}
